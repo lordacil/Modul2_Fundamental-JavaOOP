aeda7ce9221a000efafbd207ff2f5a016f99fc3b
lordacil <rizkynugrahaherliyanto28@gmail.com>
2020-10-15 13:06:02 +0700

    Add Virtual Method Invocation

---
+++ src/com/Rizkynugraha/Modul2/VirtualMethodInvocation/Child.java
@@ -0,0 +1,9 @@
+package com.Rizkynugraha.Modul2.VirtualMethodInvocation;
+
+public class Child extends Parent{
+    public int z = 7;
+
+    public void getData(){
+        System.out.println("Data child : " + z);
+    }
+}
